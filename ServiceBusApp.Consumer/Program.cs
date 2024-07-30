111ab5d7534463ad5883840a0320e5b7d90a0510
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-07-30 11:45:24 +0300

    Added ConsumerQueue

--- ServiceBusApp.Consumer/Program.cs
+++ ServiceBusApp.Consumer/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using ServicebusApp.Common;
+using ServicebusApp.Common.Events;
+using System;
+using System.Text;
+
+namespace ServicebusApp.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeQueue<OrderCreatedEvent>(Constants.OrderCreatedQueueName, i =>
+            {
+                Console.WriteLine($"OrderCreatedEvent ReceivedMessage with id: {i.Id}, Name: {i.ProductName}");
+            }).Wait();
+
+            ConsumeQueue<OrderDeletedEvent>(Constants.OrderDeletedQueueName, i =>
+            {
+                Console.WriteLine($"OrderDeletedEvent ReceivedMessage with id: {i.Id}");
+            }).Wait();
+
+            Console.ReadLine();
+        }
+
+        private static async Task ConsumeQueue<T>(string queueName, Action<T> receivedAction)
+        {
+            IQueueClient client = new QueueClient(Constants.ConnectionString, queueName);
+
+            client.RegisterMessageHandler(async (message, ct) =>
+            {
+                var model = JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(message.Body));
+
+                receivedAction(model);
+
+                await Task.CompletedTask;
+            },
+            new MessageHandlerOptions(i => Task.CompletedTask));
+
+            Console.WriteLine($"{typeof(T).Name} is listening...");
+        }
+    }
+}
